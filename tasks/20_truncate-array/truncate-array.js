8c9c91a522a8202fef0d7ac6d9522d92db46c35c
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-10-18 13:37:06 +0300

    20 task fix

--- tasks/20_truncate-array/truncate-array.js
+++ tasks/20_truncate-array/truncate-array.js
@@ -1,6 +1,6 @@
 /* eslint "require-jsdoc": 0 */
 
 
-export function truncateArray(arr,n) {
- return n === 0 ?  arr :  arr.slice(n);
+export function truncateArray(arr, n) {
+  return n === 0 ? arr : arr.slice(n);
 }
