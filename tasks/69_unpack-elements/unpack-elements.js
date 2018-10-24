b217c9a90de45bf26903d3fa5f37a30b87ed10c0
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-10-24 13:00:18 +0300

    69 fix

--- tasks/69_unpack-elements/unpack-elements.js
+++ tasks/69_unpack-elements/unpack-elements.js
@@ -1,23 +1,14 @@
 /* eslint "require-jsdoc": 0 */
-// i'll fix this one
-export function unpackArray(arr) {
-  let notFiltred = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i]) && arr[i].length) {
-      notFiltred.push(arr[i]);
 
-      if (Array.isArray(notFiltred[i])) {
-        let curr = notFiltred[i];
-        curr = curr.toString().split(',');
-        notFiltred.splice(i, 1);
-        notFiltred = notFiltred.concat(curr);
-      }
-    } else notFiltred.push(arr[i]);
+export function unpackArray(arr, res = []) {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) && !arr[i].length) continue;
+    if (Array.isArray(arr[i])) {
+      unpackArray(arr[i], res);
+    } else {
+      res.push(arr[i]);
+    }
   }
-  notFiltred = notFiltred.map((el) => {
-    if (Number(el)) return Number(el);
-    else return el;
-  });
-  let res = notFiltred.filter((el) => !Array.isArray(el));
   return res;
 }
+
