4e2519d2f12fd5101bd2e91f71c0848b9344db59
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-10-24 13:01:59 +0300

    42fix

--- tasks/42_first-n-smallest/first-n-smallest.js
+++ tasks/42_first-n-smallest/first-n-smallest.js
@@ -1,11 +1,10 @@
 /* eslint "require-jsdoc": 0 */
 
 export function firstNSmallest(arr, n) {
-  for (let i = 0; n!==arr.lengh; i++) {
-    if (arr.length===n) break;
-    if (n===0) return [];
-    let theBiggest = Math.max(...arr);
-    arr.splice(arr.lastIndexOf(theBiggest), 1);
+  let arrClone = [...arr];
+  if (!n > arrClone.length) return undefined;
+  while (arrClone.length !== n) {
+    arrClone.splice(arrClone.lastIndexOf(Math.max.apply(null, arrClone)), 1);
   }
-  return arr;
+  return arrClone;
 }
