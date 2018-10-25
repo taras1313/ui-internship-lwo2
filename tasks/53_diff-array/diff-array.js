e7adb29747dd75c1c8203d1106de7bea9f143414
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-10-25 17:30:21 +0300

    fixed

--- tasks/53_diff-array/diff-array.js
+++ tasks/53_diff-array/diff-array.js
@@ -2,7 +2,6 @@
 
 
 export function diffArray(arr1, arr2) {
-  // let res = [];
   let fromFirst = arr1.filter((el) => arr2.indexOf(el) === -1);
   let fromSecond = arr2.filter((el) => arr1.indexOf(el) === -1);
   return fromFirst.concat(fromSecond);
