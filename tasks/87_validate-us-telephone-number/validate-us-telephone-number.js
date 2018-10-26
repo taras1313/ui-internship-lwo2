f28d21d208ef4ea7e5c78ab78fc4ed48b5c894b9
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-10-26 15:57:31 +0300

    fixed

--- tasks/87_validate-us-telephone-number/validate-us-telephone-number.js
+++ tasks/87_validate-us-telephone-number/validate-us-telephone-number.js
@@ -1,6 +1,7 @@
 /* eslint "require-jsdoc": 0 */
 export function telephoneCheck(num) {
   /* eslint max-len: [ "error", { "code" : 100 }] */
+  /* eslint-disable-next-line */
   const regExp = /^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s\-]?[\0-9]{3}[\s\-]?[0-9]{4}$/im;
   return num.match(regExp) ? true : false;
 }
