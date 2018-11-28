e34042fa35f506d9519bfad3357ab015938b9ca9
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-11-28 19:50:55 +0200

    eslint

--- css-practice/task3/workspace/accordeon.js
+++ css-practice/task3/workspace/accordeon.js
@@ -1,60 +1,23 @@
+/* global document */
 const prevEvent = {
   prev: null,
 };
-
 const accordrionContainer = document.querySelector('.workflow-info');
 
 accordrionContainer.addEventListener('click', function(e) {
-  const currentPosition = e.target;
-  currentPosition.onclick = function(e) {
-    if (this.nextElementSibling.classList.contains('descr')) {
-      // this.nextElementSibling.classList.toggle('show-block');
-      
-      if(!this.nextElementSibling.classList.contains('show-block')) {
-        this.nextElementSibling.classList.add('show-block');
-
-        if(prevEvent.prev) {
+  e.target.onclick = function() {
+    const content = this.nextElementSibling;
+    if (content.classList.contains('descr')) {
+      if (!content.classList.contains('show-block')) {
+        content.classList.add('show-block');
+        if (prevEvent.prev) {
           prevEvent.prev.classList.remove('show-block');
         }
-
-        prevEvent.prev = this.nextElementSibling;
+        prevEvent.prev = content;
       } else {
-        this.nextElementSibling.classList.remove('show-block');
+        content.classList.remove('show-block');
         prevEvent.prev = null;
       }
-  }
-} 
+    }
+  };
 }, true);
-
-
-// accordrionContainer.addEventListener('click', function(e) {
-//   const test = e.path.forEach((el) => {
-//     if(el.classList.contains('activity')) {
-//       el.innerText = 'govno';
-//     }
-//   })
-//   console.log(test);
-// }, true)
-
-
-
-// accord.onclick = (e) => {
-  // const curr = e.target;
-  // const currP = curr.querySelector('p');
-
-  // if (currP.classList.contains('d-none')) {
-  //   currP.classList.remove('d-none');
-  //   curr.style.height = '100px';
-
-  //   if (prevEvent.prev) {
-  //     prevEvent.prev.style.height = '30px';
-  //     prevEvent.prev.querySelector('p').classList.add('d-none');
-  //   }
-  //   prevEvent.prev = curr;
-  // } else {
-  //   currP.classList.add('d-none');
-  //   curr.style.height = '30px';
-  //   prevEvent.prev = null;
-  // }
-// };
-
