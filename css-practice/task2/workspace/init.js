2535e5148bc3f48171305a71a3f34b4e777d5642
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-12-03 19:20:21 +0200

    fixed comments

--- css-practice/task2/workspace/init.js
+++ css-practice/task2/workspace/init.js
@@ -1,4 +1,9 @@
+/* global document */
 import test from './videoplayer.js';
+import validate from './form-validation.js';
+
 const videoHandler = document.querySelector('.video-player');
+const subscribeFrom = document.querySelector('form');
 
 videoHandler.addEventListener('click', test.videoHandler);
+subscribeFrom.addEventListener('keyup', validate, true);
