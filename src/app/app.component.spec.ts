e5c0e17e728589e8812e679baa62dee587bbd011
taras1313 <38490294+taras1313@users.noreply.github.com>
2018-12-24 14:29:30 +0200

    [TARAS YAKYMIUK] Module share styleguide (#223)
    * shared module + core module added
    
    * shared module + core module added, lint fixed
    
    * final test fixes
    
    * fixed comments
    
    * fixed forRoot and other comments

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { HeaderNavComponent } from './header/header-nav/header-nav.component';
-import { HeaderContactsComponent } from './header/header-contacts/header-contacts.component';
+
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -15,18 +12,7 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        HeaderComponent,
-        FooterComponent,
-        MainComponent,
-        HeaderNavComponent,
-        HeaderContactsComponent
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { HeaderContactsComponent } from './header/header-contacts/header-contacts.component';
-import { HeaderNavComponent } from './header/header-nav/header-nav.component';
+
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent,
-    HeaderContactsComponent,
-    HeaderNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {  }
