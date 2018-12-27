015ea31b01be8ba6519b955487b5aac58535f9d4
NFalinska <nataliia.falinska@gmail.com>
2018-12-27 18:25:20 +0200

    style fixed

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import { RequestOptions } from 'https';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { RequestOptions } from 'http';
 import { Observable } from 'rxjs';
-import { catchError} from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 import { environment } from '../environments/environment';
@@ -13,7 +13,7 @@ import { environment } from '../environments/environment';
 export class DataService {
   private baseURL: String = environment.baseURL;
   private headers: HttpHeaders = new HttpHeaders({
-    'Content-Type':  'application/json'
+    'Content-Type': 'application/json'
   });
 
   constructor(private http: HttpClient) { }
@@ -21,24 +21,28 @@ export class DataService {
   public create(path: string, options: RequestOptions): Observable<any> {
     return this.sendRequest('post', path, options);
   }
-  
+
   public delete(path: string, options: RequestOptions): Observable<any> {
     return this.sendRequest('delete', path, options);
   }
-  
+
+  public injectHeader(key: string, value: string): void {
+    this.headers.set(key, value);
+  }
+
   public ejectHeader(key): void {
     this.headers.delete(key);
   }
-  
-  public get(path:string, options:RequestOptions): Observable<any> {
+
+  public get(path: string, options: RequestOptions): Observable<any> {
     return this.sendRequest('get', path, options);
   }
 
   public update(path: string, options: RequestOptions): Observable<any> {
     return this.sendRequest('put', path, options);
   }
-  
-  private getUrl(path:string): string {
+
+  private getUrl(path: string): string {
     return `${this.baseURL}/${path}`;
   }
 
@@ -52,15 +56,16 @@ export class DataService {
     }
     return throwError(
       'Something bad happened; please try again later.');
-  };
+  }
 
-  private sendRequest(method:string, path:string, options:RequestOptions): Observable<any> {
-    if(!this.http[method]) throw new Error('Method does\'nt supported in HTTPClient')
+  private sendRequest(method: string, path: string, options: RequestOptions): Observable<any> {
+    if (!this.http[method]) {
+      throw new Error('Method does\'nt supported in HTTPClient');
+    }
 
     return this.http[method](this.getUrl(path), {
       headers: this.headers,
       ...options
     }).pipe(catchError(this.handleError));
   }
-
 }
