ad1a8cf22731ab1b6aa82855eae11aa9ec590539
taras1313 <jakimuk.taras.semenowuch@gmail.com>
2018-12-28 12:29:51 +0200

    fixes for okapi-master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { HeaderContactsComponent } from './header/header-contacts/header-contacts.component';
-import { HeaderNavComponent } from './header/header-nav/header-nav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 
@@ -21,7 +16,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
     CoreModule,
   ],
   providers: [],
